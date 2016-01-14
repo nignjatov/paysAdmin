d886a43fc7b9f52c1617fc09897bfe0d68c4703e
nignjatov <ignjatov90@hotmail.com>
2016-01-14 21:12:30 +0100

    Categories integrated

--- angular/controllers/categoriesCtrl.js
+++ angular/controllers/categoriesCtrl.js
@@ -1,60 +1,101 @@
-angular.module('paysAdmin').controller("categoriesCtrl", ["$scope", "$rootScope","CategoryService",
-    function ($scope, $rootScope,CategoryService) {
+angular.module('paysAdmin').controller("categoriesCtrl", ["$scope", "$rootScope", "CategoryService", "Notification",
+  function ($scope, $rootScope, CategoryService, Notification) {
 
-        CategoryService.getCategories().then(function(data){
-          $scope.categories = data;
+    $scope.selectedCategory       = null;
+    $scope.selectedCategoryScope  = null;
+    $scope.selectedParentCategory = null;
+
+    $scope.loadCategories = function () {
+      CategoryService.getCategories().then(function (data) {
+        $scope.categories = data;
+        angular.forEach($scope.categories, function (rootCat) {
+          rootCat.level = 0;
+          angular.forEach(rootCat.children, function (firstLevel) {
+            firstLevel.level = 1;
+            angular.forEach(firstLevel.children, function (secondLevel) {
+              secondLevel.level    = 2;
+              secondLevel.children = [];
+            });
+          });
         });
+      });
+    }
 
-        $scope.items = [
-            {
-                "id": 1,
-                "title": "node1",
-                "nodes": [
-                    {
-                        "id": 11,
-                        "title": "node1.1",
-                        "nodes": [
-                            {
-                                "id": 111,
-                                "title": "node1.1.1",
-                                "nodes": []
-                            }
-                        ]
-                    },
-                    {
-                        "id": 12,
-                        "title": "node1.2",
-                        "nodes": []
-                    }
-                ]
-            },
-            {
-                "id": 2,
-                "title": "node2",
-                "nodrop": true,
-                "nodes": [
-                    {
-                        "id": 21,
-                        "title": "node2.1",
-                        "nodes": []
-                    },
-                    {
-                        "id": 22,
-                        "title": "node2.2",
-                        "nodes": []
-                    }
-                ]
-            },
-            {
-                "id": 3,
-                "title": "node3",
-                "nodes": [
-                    {
-                        "id": 31,
-                        "title": "node3.1",
-                        "nodes": []
-                    }
-                ]
-            }
-        ]
-    }]);
+    $scope.canHaveSubcategories = function (scope) {
+      if (scope.$modelValue.level < 2) {
+        return true;
+      }
+      return false;
+    }
+
+    $scope.canBeDeleted = function (scope) {
+      if (scope.$modelValue.children && scope.$modelValue.children.length > 0) {
+        return false;
+      } else {
+        return true;
+      }
+
+    }
+
+    $scope.newSubCategory = function (scope) {
+      $scope.selectedParentCategory = scope.$modelValue;
+      $scope.selectedCategory       = {
+        level: $scope.selectedParentCategory.level + 1,
+        name: "",
+        products: [],
+        children: []
+      }
+      if ($scope.selectedCategory < 2) {
+        $scope.selectedCategory.children = null;
+      }
+    };
+
+    $scope.removeCategory = function (scope) {
+      CategoryService.deleteCategory(scope.$modelValue.id)
+        .then(function (data) {
+          Notification.success("Category deleted");
+          scope.remove();
+        }).catch(function (err) {
+          Notification.error("Failed to delete category");
+        });
+    };
+
+    $scope.saveCategory = function () {
+      if ($scope.selectedCategory.id) {
+        CategoryService.updateCategory($scope.selectedCategory.id,
+          {
+            name: $scope.selectedCategory.name
+          }).then(function (data) {
+            Notification.success("Category updated");
+            $scope.clearSelectedCategory();
+          }).catch(function (err) {
+            Notification.error("Failed to update category");
+          });
+      } else {
+        CategoryService.createCategory({
+          name: $scope.selectedCategory.name,
+          supercategory: $scope.selectedParentCategory.id
+        }).then(function (data) {
+          Notification.success("Category created");
+          $scope.selectedParentCategory.children.push($scope.selectedCategory);
+          $scope.clearSelectedCategory();
+          $scope.loadCategories();
+        }).catch(function (err) {
+          Notification.error("Failed to create category");
+        });
+        ;
+      }
+    };
+
+    $scope.selectCategory = function (scope) {
+      $scope.selectedCategoryScope = scope;
+      $scope.selectedCategory      = $scope.selectedCategoryScope.$modelValue;
+    }
+
+    $scope.clearSelectedCategory = function () {
+      $scope.selectedCategoryScope = null;
+      $scope.selectedCategory      = null
+    }
+
+    $scope.loadCategories();
+  }]);
--- angular/services/categoryService.js
+++ angular/services/categoryService.js
@@ -1,25 +1,85 @@
 angular.module('paysAdmin').service('CategoryService',
-     function ($rootScope, $q, $http) {
+  function ($rootScope, $q, $http) {
 
-         return {
-             getCategories : function () {
-                 var deffered = $q.defer();
-                 $http.get("product_category").
-                     success(function (data, status) {
-                         if (status == 200) {
-                             deffered.resolve(data);
-                         } else {
-                             console.log("getCategories | Status not OK " + status);
-                             deffered.reject("Error");
-                         }
+    return {
+      getCategories: function () {
+        var deffered = $q.defer();
+        $http.get("product_category").
+          success(function (data, status) {
+            if (status == 200) {
+              deffered.resolve(data);
+            } else {
+              console.log("getCategories | Status not OK " + status);
+              deffered.reject("Error");
+            }
 
-                     }).
-                     error(function (data, status) {
-                         console.log("Error " + status);
-                         deffered.reject("Error");
-                     });
+          }).
+          error(function (data, status) {
+            console.log("Error " + status);
+            deffered.reject("Error");
+          });
 
-                 return deffered.promise;
-             }
-             }
-    });
+        return deffered.promise;
+      },
+
+      createCategory: function (categoryData) {
+        var deffered = $q.defer();
+        $http.post("product_category",categoryData).
+          success(function (data, status) {
+            if (status == 200) {
+              deffered.resolve(data);
+            } else {
+              console.log("createCategory | Status not OK " + status);
+              deffered.reject("Error");
+            }
+
+          }).
+          error(function (data, status) {
+            console.log(" createCategory | Error " + status);
+            deffered.reject("Error");
+          });
+
+        return deffered.promise;
+      },
+
+      updateCategory: function (catId,categoryData) {
+        var deffered = $q.defer();
+        $http.put("product_category/"+catId,categoryData).
+          success(function (data, status) {
+            if (status == 200) {
+              deffered.resolve(data);
+            } else {
+              console.log("updateCategory | Status not OK " + status);
+              deffered.reject("Error");
+            }
+
+          }).
+          error(function (data, status) {
+            console.log(" updateCategory | Error " + status);
+            deffered.reject("Error");
+          });
+
+        return deffered.promise;
+      },
+
+      deleteCategory: function (categoryId) {
+        var deffered = $q.defer();
+        $http.delete("product_category/"+categoryId).
+          success(function (data, status) {
+            if (status == 200) {
+              deffered.resolve(data);
+            } else {
+              console.log("deleteCategory | Status not OK " + status);
+              deffered.reject("Error");
+            }
+
+          }).
+          error(function (data, status) {
+            console.log(" deleteCategory | Error " + status);
+            deffered.reject("Error");
+          });
+
+        return deffered.promise;
+      }
+    }
+  });
